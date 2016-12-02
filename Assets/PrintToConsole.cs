0f34e36f80f74cd11aee9a96367d150c8524fc91
Royston Henson <rlqhenson@hotmail.com>
2016-12-02 17:41:05 +0000

    added game logic and endgame message

--- Assets/PrintToConsole.cs
+++ Assets/PrintToConsole.cs
@@ -3,41 +3,53 @@ using System.Collections;
 
 public class PrintToConsole : MonoBehaviour {
 
+	public Vector2 playerLocation = new Vector2(8.0f, 7.0f);
+	public Vector2 playerHome = new Vector2(0.0f, 0.0f);
+	Vector2 distanceToHome;
+
 	// Use this for initialization
 	void Start () {
-		float playerLocation = 5.0f;	// Distance in meters
-		float playerHome = -1.0f;
-		float distance = playerHome - playerLocation;
+		distanceToHome = playerHome - playerLocation;
 		print ("Welcome to \"Find Your Way Home!\"");
 		print ("You have woken up after a crazy night out");
 		print("and now need to find your way home.");
 		print("Location: " + playerLocation);
-		print("Distance: " + distance);
-		if (playerLocation > playerHome) {
-			print("Go backwards!");
-		}
-		if (playerLocation < playerHome) {
-			print("Go forwards!");
-		}
-		if (playerLocation == playerHome) {
-		print("Congradulations!! You made it home!!");
-		}
+		print("Distance to home: " + distanceToHome.magnitude);
 
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			print("Up key pressed");
+			playerLocation = playerLocation + new Vector2(1.0f, 0);
+			print("Location: " + playerLocation);
+			print("You have chosen to move up one");
+			distanceToHome = playerHome - playerLocation;
+			print("Distance from home: " + distanceToHome);
 		}
 		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			print("Down key pressed");
+			playerLocation = playerLocation + new Vector2(-1.0f, 0);
+			print("Location: " + playerLocation);
+			print("You have chosen to move down one");
+			distanceToHome = playerHome - playerLocation;
+			print("Distance from home: " + distanceToHome);
 		}
 		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-			print("Left key pressed");
+			playerLocation = playerLocation + new Vector2(0, -1.0f);
+			print("Location: " + playerLocation);
+			print("You have chosen to move left one");
+			distanceToHome = playerHome - playerLocation;
+			print("Distance from home: " + distanceToHome);
 		}
 		if (Input.GetKeyDown(KeyCode.RightArrow)) {
-			print("Right key pressed");
+			playerLocation = playerLocation + new Vector2(0, 1.0f);
+			print("Location: " + playerLocation);
+			print("You have chosen to move right one");
+			distanceToHome = playerHome - playerLocation;
+			print("Distance from home: " + distanceToHome);
+		}
+		if (playerLocation == playerHome) {
+		print("Congratulations!! You made it home!!");
 		}
 	}
 }
