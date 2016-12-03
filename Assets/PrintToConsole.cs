8211b5f6cac9800e7c5cba6132f5a15d26182ace
Royston Henson <rlqhenson@hotmail.com>
2016-12-03 12:16:28 +0000

    refactored PrintToConsole

--- Assets/PrintToConsole.cs
+++ Assets/PrintToConsole.cs
@@ -8,47 +8,31 @@ public class PrintToConsole : MonoBehaviour {
 	Vector2 distanceToHome;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		distanceToHome = playerHome - playerLocation;
-		print ("Welcome to \"Find Your Way Home!\"");
-		print ("You have woken up after a crazy night out");
+		print("Welcome to \"Find Your Way Home!\"");
+		print("You have woken up after a crazy night out");
 		print("and now need to find your way home.");
-		print("Location: " + playerLocation);
-		print("Distance to home: " + distanceToHome.magnitude);
-
+		print("Your location: " + playerLocation);
+		print("Distance to your home: " + distanceToHome.magnitude)
 	}
 
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			playerLocation = playerLocation + new Vector2(1.0f, 0);
-			print("Location: " + playerLocation);
-			print("You have chosen to move up one");
+	void Update() {
+		checkPLayerLocation (KeyCode.UpArrow, new Vector2(1.0f, 0));
+		checkPLayerLocation (KeyCode.DownArrow, new Vector2(-1.0f, 0));
+		checkPLayerLocation (KeyCode.LeftArrow, new Vector2(0, -1.0f));
+		checkPLayerLocation (KeyCode.RightArrow, new Vector2(0, 1.0f));
+	}
+
+	void checkPLayerLocation(KeyCode kc, Vector2 movementVector) {
+		if(Input.GetKeyDown(kc)) {
+			playerLocation = playerLocation + movementVector;
+			print("Your location: " + playerLocation);
 			distanceToHome = playerHome - playerLocation;
-			print("Distance from home: " + distanceToHome);
+			print("Distance from your home: " + distanceToHome);
 		}
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			playerLocation = playerLocation + new Vector2(-1.0f, 0);
-			print("Location: " + playerLocation);
-			print("You have chosen to move down one");
-			distanceToHome = playerHome - playerLocation;
-			print("Distance from home: " + distanceToHome);
-		}
-		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-			playerLocation = playerLocation + new Vector2(0, -1.0f);
-			print("Location: " + playerLocation);
-			print("You have chosen to move left one");
-			distanceToHome = playerHome - playerLocation;
-			print("Distance from home: " + distanceToHome);
-		}
-		if (Input.GetKeyDown(KeyCode.RightArrow)) {
-			playerLocation = playerLocation + new Vector2(0, 1.0f);
-			print("Location: " + playerLocation);
-			print("You have chosen to move right one");
-			distanceToHome = playerHome - playerLocation;
-			print("Distance from home: " + distanceToHome);
-		}
-		if (playerLocation == playerHome) {
+		if(playerLocation == playerHome) {
 		print("Congratulations!! You made it home!!");
 		}
 	}
